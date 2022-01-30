96edbfb5233d9725e4d249c7cb0667c411a7a2b6
Mauricio Barva <mauriciobarva@gmail.com>
2022-01-30 15:08:17 -0500

    message model added

--- models/message.mjs
+++ models/message.mjs
@@ -1,2 +1,24 @@
-//TODO: Crear el modelo message
-// Cada nueva isntancia de este modelo tiene el mesage, la conversacion a la que pertenece y el usuario que la creó
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const messageSchema = new Schema({
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    conversation: {
+        type: Schema.Types.ObjectId,
+        ref: 'Conversation'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Message = model('Message', messageSchema);
+export default Message;
